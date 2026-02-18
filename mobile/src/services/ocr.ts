296779104f37741634f12ab8a6360f042f13226e
Jeremia Yovinus <jyov0001@student.monash.edu>
2026-02-19 00:47:12 +1100

    fix: Use expo-file-system/legacy to avoid deprecated API error

--- mobile/src/services/ocr.ts
+++ mobile/src/services/ocr.ts
@@ -1,4 +1,4 @@
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import Constants from 'expo-constants';
 import { Platform } from 'react-native';
 
